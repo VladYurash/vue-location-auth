78e7cd7879dea0f1954f81186706c816a00e5c7e
VladYurash <vlad.yurash@gmail.com>
2020-06-23 22:10:24 +0300

    some basic shit

---
+++ src/components/home/GMap.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="map">
+    <h2>Map</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'GMap',
+    data: () => ({
+
+    })
+  }
+</script>
+
+<style lang="sass">
+
+</style>
---
+++ src/components/lauout/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="deep-purple darken-1">
+    <div class="container">
+      <a class="brand-logo left">VueGeo</a>
+      <ul class="right">
+        <li><a href="">SignUp</a></li>
+        <li><a href="">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: () => ({
+
+  })
+}
+</script>
+
+<style lang="sass">
+
+</style>
